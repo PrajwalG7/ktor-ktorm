d51c64beb8312fbe8b1a5283347966a320779f23
PrajwalG7 <gaikwadaryan688@gmail.com>
2022-09-22 14:23:59 +0530

    Inserting Notes into Database with a POST request

--- src/main/kotlin/com/example/routing/NotesRouter.kt
+++ src/main/kotlin/com/example/routing/NotesRouter.kt
@@ -3,10 +3,15 @@ package com.example.routing
 import com.example.db.DatabaseConnection
 import com.example.db.entities.NotesEntity
 import com.example.models.Note
+import com.example.models.NoteRequest
+import com.example.models.NoteResponse
+import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import org.ktorm.dsl.from
+import org.ktorm.dsl.insert
 import org.ktorm.dsl.map
 import org.ktorm.dsl.select
 
@@ -24,6 +29,26 @@ fun Application.notesRoutes(){
                     Note(id?:-1,note ?:"")
                 }
           call.respond(notes)
+        }
+
+        post ("/notes"){
+            val request= call.receive<NoteRequest>()
+            val result=db.insert(NotesEntity){
+                set(it.note,request.note)
+            }
+            if(result==1){
+                //success response
+                call.respond(HttpStatusCode.OK,NoteResponse(
+                    success = true,
+                    data= "Value has been successfully inserted."
+                ))
+            }else{
+                //failure response
+                call.respond(HttpStatusCode.BadRequest,NoteResponse(
+                    success = false,
+                    data= "Failed to insert value."
+                ))
+            }
         }
     }
 }
