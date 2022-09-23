cac3a80cadfe213e8f39d019e86b7495f22dda93
PrajwalG7 <gaikwadaryan688@gmail.com>
2022-09-23 22:05:10 +0530

    Login User

--- src/main/kotlin/com/example/routing/AuthenticationRouter.kt
+++ src/main/kotlin/com/example/routing/AuthenticationRouter.kt
@@ -3,6 +3,7 @@ package com.example.routing
 import com.example.db.DatabaseConnection
 import com.example.db.entities.UserEntity
 import com.example.models.NoteResponse
+import com.example.models.User
 import com.example.models.UserCredentials
 import io.ktor.http.*
 import io.ktor.server.application.*
@@ -10,6 +11,7 @@ import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import org.ktorm.dsl.*
+import org.mindrot.jbcrypt.BCrypt
 
 fun Application.authenticationRoutes(){
 
@@ -56,6 +58,59 @@ fun Application.authenticationRoutes(){
             call.respond(HttpStatusCode.Created,NoteResponse(success = true,
             data="User has been successfully created"))
 
+        }
+
+        post("/login"){
+            val userCredentials= call.receive<UserCredentials>()
+
+            //check validation
+            if(!userCredentials.isValidCredentials()){
+                call.respond(
+                    HttpStatusCode.BadRequest,
+                    NoteResponse(
+                        success = false,
+                        data="username should be greater than 3 and password should be greater than 6"
+                    )
+                )
+                return@post
+            }
+
+            val username= userCredentials.username.lowercase()
+            val password= userCredentials.password
+
+            //check if user exists
+            val user = db.from(UserEntity)
+                .select()
+                .where {
+                    UserEntity.username.eq(username)
+                }
+                .map {
+                    val id = it[UserEntity.id]!!
+                    val username=it[UserEntity.username]!!
+                    val password= it[UserEntity.password]!!
+                    User(id,username, password)
+                }.firstOrNull()
+
+            if (user==null){
+                call.respond(HttpStatusCode.BadRequest,
+                    NoteResponse(
+                        success = false,
+                        data = "Invalid username or password."
+                    ))
+            }
+            val doesPasswordMatch= BCrypt.checkpw(password,user?.password)
+            if(!doesPasswordMatch){
+                call.respond(HttpStatusCode.BadRequest,
+                    NoteResponse(
+                        success = false,
+                        data = "Invalid username or password."
+                    ))
+            }
+            call.respond(HttpStatusCode.BadRequest,
+                NoteResponse(
+                    success = true,
+                    data = "User successfully logged in."
+                ))
         }
     }
 }
